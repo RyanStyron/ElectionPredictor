d736cfc8de1bc171c47e7f49bbb7cd753b9780b4
jameswu05 <jameswu382@gmail.com>
2024-07-02 12:38:02 -0400

    trying to convert HTML parsed data into json format

--- test/webscraping.py
+++ test/webscraping.py
@@ -1,7 +1,26 @@
 from bs4 import BeautifulSoup
+import json
 
+"""
 contents = open('data_files/Polls_FiveThirtyEight.html', 'r', encoding='utf-8')
 read_contents = contents.read()
 soup = BeautifulSoup(read_contents, 'html.parser')
 
-print(soup.prettify())
+pretty_soup = soup.prettify()
+print(pretty_soup)
+"""
+
+with open('data_files/Polls_FiveThirtyEight.html', 'r', encoding='utf-8') as contents:
+    read_contents = contents.read()
+
+soup = BeautifulSoup(read_contents, 'html.parser')
+
+script_tag = soup.find('script', {'type': 'application/json'})
+
+if script_tag:
+    data = script_tag.string.strip()
+    json_data = json.loads(data)
+    pretty_json = json.dumps(json_data, indent=4)
+    print(pretty_json)
+else:
+    print("No script tag found")
