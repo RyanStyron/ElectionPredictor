a3f71b803b28663615085f4ddc597b4077489906
jameswu05 <jameswu382@gmail.com>
2024-07-05 15:15:19 -0400

    used html_to_json library and wrote contents into separate file

--- test/webscraping.py
+++ test/webscraping.py
@@ -1,26 +1,16 @@
 from bs4 import BeautifulSoup
 import json
+import html_to_json
 
-"""
 contents = open('data_files/Polls_FiveThirtyEight.html', 'r', encoding='utf-8')
 read_contents = contents.read()
 soup = BeautifulSoup(read_contents, 'html.parser')
 
 pretty_soup = soup.prettify()
-print(pretty_soup)
-"""
+json_soup = html_to_json.convert(pretty_soup);
 
-with open('data_files/Polls_FiveThirtyEight.html', 'r', encoding='utf-8') as contents:
-    read_contents = contents.read()
+with open('fivethirtyeight.txt', 'w') as outfile:
+    outfile.write(pretty_soup)
 
-soup = BeautifulSoup(read_contents, 'html.parser')
 
-script_tag = soup.find('script', {'type': 'application/json'})
-
-if script_tag:
-    data = script_tag.string.strip()
-    json_data = json.loads(data)
-    pretty_json = json.dumps(json_data, indent=4)
-    print(pretty_json)
-else:
-    print("No script tag found")
+# print(json_soup['cycles'])
