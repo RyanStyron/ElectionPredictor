06cf61eccfaf44de5d6352257910190203d4c123
jameswu05 <jameswu382@gmail.com>
2024-07-10 15:00:14 -0400

    trying to extract nevessary data and store in dictionary

--- test/webscraping_rcp.py
+++ test/webscraping_rcp.py
@@ -1,12 +1,23 @@
 from bs4 import BeautifulSoup
+import requests
 import json
 import html_to_json
 
-contents = open('data_files/Polls_RealClearPolitics.html', 'r', encoding='utf-8')
-read_contents = contents.read()
-soup = BeautifulSoup(read_contents, 'html.parser')
+def extractFromWeb(url):
+    page = requests.get(url)
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.text, 'html.parser')
+        extracted_data = str(soup.find_all('script'))
+        return extracted_data
 
-pretty_soup = soup.prettify()
+def storeInformation(data):
+    poll_dict = {}
+    poll_dict["pollster"] = # need to figure out how to parse
 
-with open('realclearpolitics.txt', 'w') as outfile:
-    outfile.write(pretty_soup)
+
+url = 'https://www.realclearpolling.com/polls/president/general/2024/trump-vs-biden'
+data = extractFromWeb(url)
+
+with open('rcp_data.txt', 'w') as outfile:
+    outfile.write(data)
+
